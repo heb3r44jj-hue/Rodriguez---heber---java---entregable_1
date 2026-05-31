b3e419946918e93c0cccd4e51130613a1857aad2
heber <heb3r44jj@gmail.com>
2026-05-31 19:19:22 -0400

    Clase Persona

--- Persona.java
+++ Persona.java
@@ -1,4 +1,26 @@
 public abstract class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Persona(String nombre, String apellido, int edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public abstract String obtenerInformacion();
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
+
+    public String getnombre() { return nombre; }
+    public void setnombre(String nombre) { this.nombre = nombre; }
     
-    
+    public String getapellido(){ return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
 }
